88192bbd798027a6d8f65df41a53f9210fb97509
dorian <ddoussain@gmail.com>
2020-10-13 13:12:03 +0200

    mosaic netflix

---
+++ index.js
@@ -0,0 +1,8 @@
+const figure = $(".video").hover( hoverVideo, hideVideo );
+function hoverVideo(e) {  
+    $('video', this).get(0).play(); 
+}
+
+function hideVideo(e) {
+    $('video', this).get(0).pause(); 
+}
